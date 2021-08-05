cb936b79716725d02a31362cd11d07569abb772d
Ankit Pathak <ankitpathak143192@gmail.com>
2021-08-05 11:43:48 +0530

    .

--- components/About/Section1.js
+++ components/About/Section1.js
@@ -1,7 +1,7 @@
 function Section1() {
     return (
-        <section style={{maxHeight: "70vh"}} className="relative w-full overflow-hidden">
-            <img className="w-full z-2" src="/images/design/park3.jpg"/>
+        <section style={{height: "70vh"}} className="relative w-full overflow-hidden">
+            <img className="w-full h-full object-cover z-2" src="/images/design/park3.jpg"/>
             <div className="absolute z-10 bottom-10 p-4 md:px-24 text-white">
                 <h3 className="font-bold cursive text-5xl">Live Free</h3>
                 <h3 className="basic font-extrabold text-7xl uppercase">At Udaan Property</h3>
